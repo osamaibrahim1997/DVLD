14f52ec5bf58ce8360ad26a62fe941e72873b935
osamaIbrahim <poco.osama.1.2.1.2@gmail.com>
2026-05-27 15:36:43 +0300

    Add Windows Registry support for (Remember Me) feature

--- DVLD/Global clases/clsGlobal.cs
+++ DVLD/Global clases/clsGlobal.cs
@@ -1,4 +1,5 @@
 ﻿using DVLD_Business;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,6 +17,32 @@ namespace DVLD.Global_clases
 
 
         public static bool RememberUsernameAndPassword(string Username, string Password)
+        {
+            try
+            {
+                string KeyPath = @"HKEY_CURRENT_USER\Software\DVLD";
+
+                if (Username == "")
+                {
+                    Registry.SetValue(KeyPath, "UserName", "");
+                    Registry.SetValue(KeyPath, "Password", "");
+
+                    return true;
+                }
+
+                Registry.SetValue(KeyPath, "UserName", Username);
+                Registry.SetValue(KeyPath, "Password", Password);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool RememberUsernameAndPassword1(string Username, string Password)
         {
 
             try
@@ -57,47 +84,65 @@ namespace DVLD.Global_clases
 
         public static bool GetStoredCredential(ref string Username, ref string Password)
         {
-            //this will get the stored username and password and will return true if found
-            //and false if not found.
             try
             {
-                //gets the current project's directory
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                string KeyPath = @"HKEY_CURRENT_USER\Software\DVLD";
 
-                // Path for the file that contains the credential.
-                string filePath = currentDirectory + "\\data.txt";
+                Username = Registry.GetValue(KeyPath, "UserName", "")?.ToString();
+                Password = Registry.GetValue(KeyPath, "Password", "")?.ToString();
 
-                // Check if the file exists before attempting to read it
-                if (File.Exists(filePath))
-                {
-                    // Create a StreamReader to read from the file
-                    using (StreamReader reader = new StreamReader(filePath))
-                    {
-                        // Read data line by line until the end of the file
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            Console.WriteLine(line); // Output each line of data to the console
-                            string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
-
-                            Username = result[0];
-                            Password = result[1];
-                        }
-                        return true;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                return (!string.IsNullOrEmpty(Username));
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"An error occurred: {ex.Message}");
+                MessageBox.Show(ex.Message);
                 return false;
             }
-
         }
+
+        //public static bool GetStoredCredential(ref string Username, ref string Password)
+        //{
+        //    //this will get the stored username and password and will return true if found
+        //    //and false if not found.
+        //    try
+        //    {
+        //        //gets the current project's directory
+        //        string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+        //        // Path for the file that contains the credential.
+        //        string filePath = currentDirectory + "\\data.txt";
+
+        //        // Check if the file exists before attempting to read it
+        //        if (File.Exists(filePath))
+        //        {
+        //            // Create a StreamReader to read from the file
+        //            using (StreamReader reader = new StreamReader(filePath))
+        //            {
+        //                // Read data line by line until the end of the file
+        //                string line;
+        //                while ((line = reader.ReadLine()) != null)
+        //                {
+        //                    Console.WriteLine(line); // Output each line of data to the console
+        //                    string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+
+        //                    Username = result[0];
+        //                    Password = result[1];
+        //                }
+        //                return true;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"An error occurred: {ex.Message}");
+        //        return false;
+        //    }
+
+        //}
 
     }
 }
